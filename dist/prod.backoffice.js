dc5215e32a1ce619320b21a9b0764e5012296cf6
Pablo Santos <pablo.santos@outsystems.com>
2022-01-18 10:16:36 +0000

    New Release

--- dist/prod.backoffice.js
+++ dist/prod.backoffice.js
@@ -1,4 +1,4 @@
-/*! prod.backoffice.js || Version: 5.1.128006 || Generated: Tue Jan 18 2022 10:15:48 GMT+0000 (Western European Standard Time) */
+/*! prod.backoffice.js || Version: 5.1.128007 || Generated: Tue Jan 18 2022 10:16:26 GMT+0000 (Western European Standard Time) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
