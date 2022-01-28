c0cee81163d2c1cee5d61c477625465afe4aee8b
DiogoRibeiro777 <diogo.ribeiro@outsystems.com>
2022-01-28 17:20:15 +0000

    New Release

--- dist/prod.backoffice.js
+++ dist/prod.backoffice.js
@@ -1,4 +1,4 @@
-/*! prod.backoffice.js || Version: 5.1.128026 || Generated: Fri Jan 28 2022 17:18:43 GMT+0000 (Western European Standard Time) */
+/*! prod.backoffice.js || Version: 5.1.128027 || Generated: Fri Jan 28 2022 17:20:03 GMT+0000 (Western European Standard Time) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
