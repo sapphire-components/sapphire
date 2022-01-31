2884d0945c3e906fe79c5b50e8b834d0cf98e192
Maria Costa <maria.costa@outsystems.com>
2022-01-31 16:43:44 +0000

    New Release

--- dist/prod.backoffice.js
+++ dist/prod.backoffice.js
@@ -1,4 +1,4 @@
-/*! prod.backoffice.js || Version: 5.1.128031 || Generated: Mon Jan 31 2022 12:25:52 GMT+0000 (Western European Standard Time) */
+/*! prod.backoffice.js || Version: 5.1.128031 || Generated: Mon Jan 31 2022 16:43:39 GMT+0000 (Western European Standard Time) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
