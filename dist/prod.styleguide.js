8fd16e9f4af49e4ad5de53c37ce75db4371551f3
Maria Costa <maria.costa@outsystems.com>
2022-01-31 12:17:07 +0000

    New Release

--- dist/prod.styleguide.js
+++ dist/prod.styleguide.js
@@ -1,4 +1,4 @@
-/*! prod.styleguide.js || Version: 5.1.128029 || Generated: Mon Jan 31 2022 12:15:31 GMT+0000 (Western European Standard Time) */
+/*! prod.styleguide.js || Version: 5.1.128029 || Generated: Mon Jan 31 2022 12:17:02 GMT+0000 (Western European Standard Time) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
