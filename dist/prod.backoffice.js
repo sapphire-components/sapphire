2aaa91c06ce03c29f2506eef742d8cbd331bfd0f
hugocodinha <hugo.codinha@outsystems.com>
2022-03-30 18:04:24 +0100

    New Release

--- dist/prod.backoffice.js
+++ dist/prod.backoffice.js
@@ -1,4 +1,4 @@
-/*! prod.backoffice.js || Version: 5.1.128051 || Generated: Wed Mar 30 2022 17:35:13 GMT+0100 (Western European Summer Time) */
+/*! prod.backoffice.js || Version: 5.1.128051 || Generated: Wed Mar 30 2022 18:04:17 GMT+0100 (Western European Summer Time) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
