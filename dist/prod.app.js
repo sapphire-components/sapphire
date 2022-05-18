03fbd26d7634d348dd482cf0eabb4a5f7418d850
hugocodinha <hugo.codinha@outsystems.com>
2022-05-18 16:51:35 +0100

    New Release

--- dist/prod.app.js
+++ dist/prod.app.js
@@ -1,4 +1,4 @@
-/*! prod.app.js || Version: 5.1.128056 || Generated: Tue May 17 2022 17:44:50 GMT+0100 (Western European Summer Time) */
+/*! prod.app.js || Version: 5.1.128056 || Generated: Wed May 18 2022 16:51:27 GMT+0100 (Western European Summer Time) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
