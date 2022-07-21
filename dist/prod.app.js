0b36a7aefc8cbc04c9f037e396fc23aacd212eb1
António Carvalho <amcscarvalho@gmail.com>
2022-07-21 15:10:12 +0100

    New Release

--- dist/prod.app.js
+++ dist/prod.app.js
@@ -1,4 +1,4 @@
-/*! prod.app.js || Version: 5.1.128065 || Generated: Thu Jul 21 2022 15:09:43 GMT+0100 (Western European Summer Time) */
+/*! prod.app.js || Version: 5.1.128066 || Generated: Thu Jul 21 2022 15:10:09 GMT+0100 (Western European Summer Time) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
