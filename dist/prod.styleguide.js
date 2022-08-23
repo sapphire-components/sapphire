63ec0ad254fdfe4ade984f22ab6b1257e4742c6d
hugocodinha <hugo.codinha@outsystems.com>
2022-08-23 11:31:41 +0100

    New Release

--- dist/prod.styleguide.js
+++ dist/prod.styleguide.js
@@ -1,4 +1,4 @@
-/*! prod.styleguide.js || Version: 5.1.128078 || Generated: Thu Aug 18 2022 12:14:57 GMT+0100 (Western European Summer Time) */
+/*! prod.styleguide.js || Version: 5.1.128078 || Generated: Tue Aug 23 2022 11:31:29 GMT+0100 (Western European Summer Time) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
