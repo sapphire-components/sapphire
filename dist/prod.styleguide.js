4ed442fffba82e9fa1b7e069c099cf3fede3bbaa
DiogoRibeiro777 <diogo.ribeiro@outsystems.com>
2023-03-16 14:42:54 +0000

    New Release

--- dist/prod.styleguide.js
+++ dist/prod.styleguide.js
@@ -1,4 +1,4 @@
-/*! prod.styleguide.js || Version: 5.1.128107 || Generated: Thu Mar 16 2023 14:42:14 GMT+0000 (Western European Standard Time) */
+/*! prod.styleguide.js || Version: 5.1.128108 || Generated: Thu Mar 16 2023 14:42:46 GMT+0000 (Western European Standard Time) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
