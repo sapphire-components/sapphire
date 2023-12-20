0e654aba2d95e4375ddb3fb7e824b66d53508ca7
DiogoRibeiro777 <diogo.ribeiro@outsystems.com>
2023-12-20 11:01:04 +0000

    New Release

--- dist/prod.app.js
+++ dist/prod.app.js
@@ -1,4 +1,4 @@
-/*! prod.app.js || Version: 5.1.128151 || Generated: Wed Dec 20 2023 11:00:19 GMT+0000 (Hora padrão da Europa Ocidental) */
+/*! prod.app.js || Version: 5.1.128152 || Generated: Wed Dec 20 2023 11:00:58 GMT+0000 (Hora padrão da Europa Ocidental) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
