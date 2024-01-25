9e236b8f82581b2c4df1117096cf1ca6e9463d7f
Ricardo Saraiva <silvasaraiva@gmail.com>
2024-01-25 12:01:31 +0000

    New Release

--- dist/prod.backoffice.js
+++ dist/prod.backoffice.js
@@ -1,4 +1,4 @@
-/*! prod.backoffice.js || Version: 5.1.128156 || Generated: Thu Jan 25 2024 11:50:39 GMT+0000 (Western European Standard Time) */
+/*! prod.backoffice.js || Version: 5.1.128157 || Generated: Thu Jan 25 2024 12:01:22 GMT+0000 (Western European Standard Time) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
