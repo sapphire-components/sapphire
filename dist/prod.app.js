7d52cdee63b3941cc99c540bb5a0ac87a56788af
Ricardo Saraiva <silvasaraiva@gmail.com>
2024-01-25 11:49:09 +0000

    New Release

--- dist/prod.app.js
+++ dist/prod.app.js
@@ -1,4 +1,4 @@
-/*! prod.app.js || Version: 5.1.128155 || Generated: Thu Jan 25 2024 11:46:29 GMT+0000 (Western European Standard Time) */
+/*! prod.app.js || Version: 5.1.128155 || Generated: Thu Jan 25 2024 11:49:01 GMT+0000 (Western European Standard Time) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
