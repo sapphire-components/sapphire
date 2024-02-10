c453828c59b366f5501783a5f83bedccfa0df30a
Ricardo Saraiva <silvasaraiva@gmail.com>
2024-02-10 00:01:44 +0000

    New Release

--- src/components/00-settings/config.js
+++ src/components/00-settings/config.js
@@ -1,6 +1,10 @@
 SapphireWidgets = window.SapphireWidgets = window.SapphireWidgets || {};
 
 const isProduction = process.env.NODE_ENV === 'production ';
+
 if (!isProduction) {
+  /*
+    Adding this class allows to build js/css specific for dev purposes.
+   */
   document.querySelector('body').classList.add('webpack-dev');
 }
