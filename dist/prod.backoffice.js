c9ea863b7d29ef2efd29b9d412a0096a372d6ae3
Ricardo Saraiva <silvasaraiva@gmail.com>
2024-04-04 16:35:30 +0100

    New Release

--- dist/prod.backoffice.js
+++ dist/prod.backoffice.js
@@ -1,4 +1,4 @@
-/*! prod.backoffice.js || Version: 5.5.24 || Generated: Thu Apr 04 2024 16:34:45 GMT+0100 (Western European Summer Time) */
+/*! prod.backoffice.js || Version: 5.5.25 || Generated: Thu Apr 04 2024 16:35:09 GMT+0100 (Western European Summer Time) */
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
