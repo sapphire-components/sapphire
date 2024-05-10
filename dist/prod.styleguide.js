3a0f7eaa1ff7844885d2e66381861ee29a44a30b
Ricardo Saraiva <silvasaraiva@gmail.com>
2024-05-10 12:44:21 +0100

    New Release

--- dist/prod.styleguide.js
+++ dist/prod.styleguide.js
@@ -1,4 +1,4 @@
-/*! prod.styleguide.js || Version: 5.5.57 || Generated: Fri May 10 2024 12:43:31 GMT+0100 (Western European Summer Time) */
+/*! prod.styleguide.js || Version: 5.5.58 || Generated: Fri May 10 2024 12:44:13 GMT+0100 (Western European Summer Time) */
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
