6fc332eb60de6d1cd8488e2f994765aea36eb00b
Ricardo Saraiva <silvasaraiva@gmail.com>
2024-07-02 14:56:00 +0100

    New Release

--- dist/prod.app.js
+++ dist/prod.app.js
@@ -1,4 +1,4 @@
-/*! prod.app.js || Version: 5.5.98 || Generated: Tue Jul 02 2024 14:55:35 GMT+0100 (Western European Summer Time) */
+/*! prod.app.js || Version: 5.5.99 || Generated: Tue Jul 02 2024 14:55:53 GMT+0100 (Western European Summer Time) */
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
