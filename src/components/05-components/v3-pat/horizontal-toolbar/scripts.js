5efdd723218a948094c3d42bf3e8a2b4ac619a4c
DiogoRibeiro777 <diogo.ribeiro@outsystems.com>
2024-07-10 15:43:27 +0100

    New Release

--- src/components/05-components/v3-pat/horizontal-toolbar/scripts.js
+++ src/components/05-components/v3-pat/horizontal-toolbar/scripts.js
@@ -58,6 +58,18 @@
 
 			window.addEventListener('ToolbarFixed', () => handleResize($widget), false);
 		}
+		//avoid users flooding the server with repeated requests
+		$('.Toolbar_container a, .Toolbar__MoreOptionsList a').on('click', function(e) {
+			if($(this).hasClass('isDisabled')){
+				e.preventDefault();
+				return;
+			}
+			e.preventDefault(); // Prevent the default click behavior
+			var href = $(this).attr('href');
+			//$(this).removeAttr('href');
+			$(this).addClass('isDisabled');
+			window.location.href = href;
+		  });
 	};
 
 	handleArrows = $widget => {
