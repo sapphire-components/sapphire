d57567aae7dcb790247cf0b621508bff013e42ad
DiogoRibeiro777 <diogo.ribeiro@outsystems.com>
2024-07-10 17:15:51 +0100

    New Release

--- dist/prod.app.js
+++ dist/prod.app.js
@@ -1,4 +1,4 @@
-/*! prod.app.js || Version: 5.5.102 || Generated: Wed Jul 10 2024 17:15:04 GMT+0100 (Hora de verão da Europa Ocidental) */
+/*! prod.app.js || Version: 5.5.103 || Generated: Wed Jul 10 2024 17:15:38 GMT+0100 (Hora de verão da Europa Ocidental) */
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
