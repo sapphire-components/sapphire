50194a97f96f6d22048a356ab0cfdb3f0c34370d
alinecaliente <alinecaliente@hotmail.de>
2024-10-03 17:30:05 +0100

    New Release

--- dist/prod.app.js
+++ dist/prod.app.js
@@ -1,4 +1,4 @@
-/*! prod.app.js || Version: 5.5.133 || Generated: Thu Oct 03 2024 17:29:04 GMT+0100 (Hora de verão da Europa Ocidental) */
+/*! prod.app.js || Version: 5.5.134 || Generated: Thu Oct 03 2024 17:29:57 GMT+0100 (Hora de verão da Europa Ocidental) */
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
