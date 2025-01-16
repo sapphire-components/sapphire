f048c9a52cbd8ba58c9ef27815a6e4f8bc0d15f2
alinecaliente <alinecaliente@hotmail.de>
2025-01-16 16:38:54 +0000

    New Release

--- dist/prod.styleguide.js
+++ dist/prod.styleguide.js
@@ -1,4 +1,4 @@
-/*! prod.styleguide.js || Version: 5.5.156 || Generated: Thu Jan 16 2025 16:38:25 GMT+0000 (Hora padrão da Europa Ocidental) */
+/*! prod.styleguide.js || Version: 5.5.157 || Generated: Thu Jan 16 2025 16:38:48 GMT+0000 (Hora padrão da Europa Ocidental) */
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
