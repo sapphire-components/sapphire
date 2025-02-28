9c0dd97bbe6458ef26c5db35e0b4a7da55b82cab
Ricardo Saraiva <silvasaraiva@gmail.com>
2025-02-28 15:52:29 +0000

    New Release

--- dist/prod.app.js
+++ dist/prod.app.js
@@ -1,4 +1,4 @@
-/*! prod.app.js || Version: 5.5.171 || Generated: Fri Feb 28 2025 15:51:24 GMT+0000 (Western European Standard Time) */
+/*! prod.app.js || Version: 5.5.172 || Generated: Fri Feb 28 2025 15:52:17 GMT+0000 (Western European Standard Time) */
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
