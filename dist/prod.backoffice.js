bd5b938181eb03857193de050c6e77519e23cf51
alinecaliente <alinecaliente@hotmail.de>
2025-03-11 15:09:49 +0000

    New Release

--- dist/prod.backoffice.js
+++ dist/prod.backoffice.js
@@ -1,4 +1,4 @@
-/*! prod.backoffice.js || Version: 5.5.177 || Generated: Tue Mar 11 2025 15:08:39 GMT+0000 (Hora padrão da Europa Ocidental) */
+/*! prod.backoffice.js || Version: 5.5.178 || Generated: Tue Mar 11 2025 15:09:41 GMT+0000 (Hora padrão da Europa Ocidental) */
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
