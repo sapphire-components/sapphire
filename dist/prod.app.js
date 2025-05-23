8f9f6d454f33c53f021c19392ddd6b467d5ec14b
Igor.Kirtak <Igor.Kirtak@esystems.fi>
2025-05-23 15:49:13 +0300

    New Release

--- dist/prod.app.js
+++ dist/prod.app.js
@@ -1,4 +1,4 @@
-/*! prod.app.js || Version: 5.5.195 || Generated: Fri May 23 2025 15:48:48 GMT+0300 (GMT+03:00) */
+/*! prod.app.js || Version: 5.5.196 || Generated: Fri May 23 2025 15:49:08 GMT+0300 (GMT+03:00) */
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
