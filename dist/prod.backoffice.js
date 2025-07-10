0dbf9deee92923d70e0bcf97661eeb6db9709613
Igor.Kirtak <Igor.Kirtak@esystems.fi>
2025-07-10 17:24:52 +0300

    New Release

--- dist/prod.backoffice.js
+++ dist/prod.backoffice.js
@@ -1,4 +1,4 @@
-/*! prod.backoffice.js || Version: 5.5.215 || Generated: Wed Jul 09 2025 17:45:09 GMT+0300 (GMT+03:00) */
+/*! prod.backoffice.js || Version: 5.5.215 || Generated: Thu Jul 10 2025 17:24:47 GMT+0300 (GMT+03:00) */
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
