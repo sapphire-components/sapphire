6f3b2a03fa701fc45ced720692c359b73e5c433f
DiogoRibeiro777 <diogo.ribeiro@outsystems.com>
2025-10-09 17:11:07 +0100

    New Release

--- dist/prod.styleguide.js
+++ dist/prod.styleguide.js
@@ -1,4 +1,4 @@
-/*! prod.styleguide.js || Version: 5.5.227 || Generated: Thu Oct 09 2025 17:09:53 GMT+0100 (Hora de verão da Europa Ocidental) */
+/*! prod.styleguide.js || Version: 5.5.228 || Generated: Thu Oct 09 2025 17:10:51 GMT+0100 (Hora de verão da Europa Ocidental) */
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
