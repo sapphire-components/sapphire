ef5fa12ef3311bdb1fdd0e26ee8eca9639229161
DiogoRibeiro777 <diogo.ribeiro@outsystems.com>
2025-10-28 14:00:06 +0000

    New Release

--- dist/prod.backoffice.js
+++ dist/prod.backoffice.js
@@ -1,4 +1,4 @@
-/*! prod.backoffice.js || Version: 5.5.238 || Generated: Tue Oct 28 2025 13:59:03 GMT+0000 (Hora padrão da Europa Ocidental) */
+/*! prod.backoffice.js || Version: 5.5.239 || Generated: Tue Oct 28 2025 13:59:51 GMT+0000 (Hora padrão da Europa Ocidental) */
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
