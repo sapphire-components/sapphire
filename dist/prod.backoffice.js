91d690f1627cce837520af94afda4dc6af5b9f2c
Antonio Carvalho <antonio.carvalho@outsystems.com>
2025-12-19 15:34:55 +0000

    New Release

--- dist/prod.backoffice.js
+++ dist/prod.backoffice.js
@@ -1,4 +1,4 @@
-/*! prod.backoffice.js || Version: 5.5.253 || Generated: Tue Dec 16 2025 17:29:36 GMT+0000 (Hora padrão da Europa Ocidental) */
+/*! prod.backoffice.js || Version: 5.5.254 || Generated: Fri Dec 19 2025 15:34:52 GMT+0000 (Hora padrão da Europa Ocidental) */
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
