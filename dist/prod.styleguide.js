670f8ea9c8fba554ea6a041b85e8db798ed5eb4f
Igor.Kirtak <Igor.Kirtak@esystems.fi>
2025-12-22 14:51:06 +0300

    New Release

--- dist/prod.styleguide.js
+++ dist/prod.styleguide.js
@@ -1,4 +1,4 @@
-/*! prod.styleguide.js || Version: 5.5.248 || Generated: Mon Dec 22 2025 14:08:42 GMT+0300 (GMT+03:00) */
+/*! prod.styleguide.js || Version: 5.5.249 || Generated: Mon Dec 22 2025 14:51:01 GMT+0300 (GMT+03:00) */
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
