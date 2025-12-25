2f9e0b7876119ccfe66e7ebc4563767821f0bbad
Igor.Kirtak <Igor.Kirtak@esystems.fi>
2025-12-25 19:51:48 +0300

    SpinnerHorizontal notify on value overflow

--- src/components/05-components/v3-pat/spinner-horizontal/scripts.js
+++ src/components/05-components/v3-pat/spinner-horizontal/scripts.js
@@ -1,6 +1,6 @@
 /* Component SpinnerHorizontal */
 SapphireWidgets.SpinnerHorizontal = {
-	create: config => {
+	create: (config) => {
 		const $input = $(`#${config.widgetId} input`);
 		const $plus = $(`#${config.widgetId} a.Plus`);
 		const $minus = $(`#${config.widgetId} a.Minus`);
@@ -13,9 +13,20 @@ SapphireWidgets.SpinnerHorizontal = {
 			$plus.attr('disabled', 'disabled');
 		}
 
-		$input.on('change', function() {
+		$input.on('change', function () {
 			val = Math.abs(parseInt(this.value, 10) || +config.minValue);
-			this.value = val > +config.maxValue ? +config.maxValue : val < +config.minValue ? +config.minValue : val;
+			// Note that val can never be less than 1 because of the above
+			if (val > +config.maxValue) {
+				val = +config.maxValue;
+				this.value = val;
+				$(`#${config.widgetId} a.NotifyOverflowMax`).click();
+			} else if (val < +config.minValue) {
+				val = +config.minValue;
+				this.value = val;
+				$(`#${config.widgetId} a.NotifyOverflowMin`).click();
+			} else {
+				this.value = val;
+			}
 
 			if (val <= +config.minValue) $minus.attr('disabled', 'disabled');
 			else $minus.removeAttr('disabled');
@@ -24,55 +35,51 @@ SapphireWidgets.SpinnerHorizontal = {
 			else $plus.removeAttr('disabled');
 		});
 	},
-	increment: function(elementId, minValue, maxValue, triggerOnChange) {
-		var _element = $(elementId)
-			.find('input[type="number"]')
-			.first();
-		if (_element.val() == undefined || _element.val() == '' || isNaN(parseFloat(_element.val()))) {
+
+	increment: function (elementId, minValue, maxValue, triggerOnChange) {
+		var _element = $(`#${elementId} input`);
+		var val = parseInt(_element.val());
+		if (val == undefined || isNaN(val)) {
 			_element.val(minValue);
 			if (triggerOnChange) {
 				_element.trigger('change');
 			}
+			$(`#${elementId} a.Minus`).attr('disabled', 'disabled');
+			$(`#${elementId} a.Plus`).removeAttr('disabled');
 		} else {
-			if (parseFloat(_element.val()) < maxValue) {
-				_element.val(parseFloat(_element.val()) + 1);
+			if (val < maxValue) {
+				_element.val(val + 1);
 				if (triggerOnChange) {
 					_element.trigger('change');
 				}
-				$(elementId)
-					.find('a.Minus')
-					.removeAttr('disabled');
+				$(`#${elementId} a.Minus`).removeAttr('disabled');
 			}
-			if (parseFloat(_element.val()) >= maxValue) {
-				$(elementId)
-					.find('a.Plus')
-					.attr('disabled', 'disabled');
+			if (parseInt(_element.val()) >= maxValue) {
+				$(`#${elementId} a.Plus`).attr('disabled', 'disabled');
 			}
 		}
 	},
-	decrement: function(elementId, minValue, triggerOnChange) {
-		var _element = $(elementId)
-			.find('input[type="number"]')
-			.first();
-		if (_element.val() == undefined || _element.val() == '' || isNaN(parseFloat(_element.val()))) {
+
+	decrement: function (elementId, minValue, triggerOnChange) {
+		var _element = $(`#${elementId} input`);
+		var val = parseInt(_element.val());
+		if (val == undefined || isNaN(val)) {
 			_element.val(minValue);
 			if (triggerOnChange) {
 				_element.trigger('change');
 			}
+			$(`#${elementId} a.Minus`).attr('disabled', 'disabled');
+			$(`#${elementId} a.Plus`).removeAttr('disabled');
 		} else {
-			if (parseFloat(_element.val()) > minValue) {
-				_element.val(parseFloat(_element.val()) - 1);
+			if (val > minValue) {
+				_element.val(val - 1);
 				if (triggerOnChange) {
 					_element.trigger('change');
 				}
-				$(elementId)
-					.find('a.Plus')
-					.removeAttr('disabled');
+				$(`#${elementId} a.Plus`).removeAttr('disabled');
 			}
-			if (parseFloat(_element.val()) <= minValue) {
-				$(elementId)
-					.find('a.Minus')
-					.attr('disabled', 'disabled');
+			if (parseInt(_element.val()) <= minValue) {
+				$(`#${elementId} a.Minus`).attr('disabled', 'disabled');
 			}
 		}
 	},
