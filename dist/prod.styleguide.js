f64fab2b8d9cfa039a0fb1cca077be97b8a43f53
Antonio Carvalho <antonio.carvalho@outsystems.com>
2026-01-08 11:45:36 +0000

    New Release

--- dist/prod.styleguide.js
+++ dist/prod.styleguide.js
@@ -1,4 +1,4 @@
-/*! prod.styleguide.js || Version: 5.5.262 || Generated: Fri Jan 02 2026 12:42:51 GMT+0000 (Hora padrão da Europa Ocidental) */
+/*! prod.styleguide.js || Version: 5.5.263 || Generated: Thu Jan 08 2026 11:45:22 GMT+0000 (Hora padrão da Europa Ocidental) */
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
