216c70ab5464132ce5384f2be20ea9a1903838c5
António Carvalho <antonio.carvalho@outsystems.com>
2026-05-15 09:23:00 +0100

    New Release

--- dist/prod.app.js
+++ dist/prod.app.js
@@ -1,4 +1,4 @@
-/*! prod.app.js || Version: 5.5.321 || Generated: Fri May 15 2026 09:09:16 GMT+0100 (Western European Summer Time) */
+/*! prod.app.js || Version: 5.5.322 || Generated: Fri May 15 2026 09:22:57 GMT+0100 (Western European Summer Time) */
 /******/ (function() { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
