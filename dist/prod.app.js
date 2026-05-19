a5651b4b78a0738a3a52ffa914e18471c58955ce
António Carvalho <antonio.carvalho@outsystems.com>
2026-05-19 17:14:53 +0100

    New Release

--- dist/prod.app.js
+++ dist/prod.app.js
@@ -1,6 +1,14 @@
-/*! prod.app.js || Version: 5.5.327 || Generated: Tue May 19 2026 12:09:02 GMT+0100 (Western European Summer Time) */
+/*! prod.app.js || Version: 5.5.328 || Generated: Tue May 19 2026 17:14:50 GMT+0100 (Western European Summer Time) */
 /******/ (function() { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
+
+/***/ "./package.json":
+/***/ (function(module) {
+
+"use strict";
+module.exports = /*#__PURE__*/JSON.parse('{"name":"sapphire","version":"5.5.328","description":"Styles and scripts for Sapphire HMS design system.","keywords":["design system","sapphire","hms","style guide","patterns","css","javascript"],"homepage":"https://dev.nordicplatforms.com/StyleGuideV2_UI/Instructions.aspx","repository":{"type":"git","url":"https://github.com/sapphire-components/sapphire"},"author":{"name":"António Carvalho","email":"antonio.carvalho@outsystems.com","url":"https://www.linkedin.com/in/carvalhoantonio/"},"scripts":{"dev":"set NODE_ENV=development && webpack-dev-server --config webpack.dev.js","prod":"set NODE_ENV=production && webpack --config webpack.prod.js","new:build":"npm run prod && git add . && git commit -m \\"New Release\\" --allow-empty && git push origin main","release:patch":"release-it patch --ci","release:minor":"release-it minor --git.requireBranch=main && npm run new:build","release:major":"release-it major --git.requireBranch=main && npm run new:build"},"license":"ISC","devDependencies":{"autoprefixer":"^10.4.23","clean-webpack-plugin":"^4.0.0","css-loader":"^7.1.2","dotenv-cli":"^11.0.0","eslint":"^9.39.2","eslint-config-prettier":"^10.1.8","eslint-plugin-prettier":"^5.5.5","mini-css-extract-plugin":"^2.10.0","postcss-loader":"^8.2.0","prettier":"^3.8.1","prettier-stylelint":"^0.4.2","release-it":"^19.2.4","sass":"^1.97.3","sass-loader":"^16.0.6","style-loader":"^4.0.0","stylelint":"^17.0.0","stylelint-config-standard-scss":"^17.0.0","stylelint-order":"^7.0.1","terser-webpack-plugin":"^5.3.16","webpack":"^5.104.1","webpack-cli":"^6.0.1","webpack-dev-middleware":"^7.4.5","webpack-dev-server":"^5.2.3","webpack-hot-middleware":"^2.26.1","webpack-merge":"^6.0.1"}}');
+
+/***/ }),
 
 /***/ "./src/components sync recursive \\.js$":
 /***/ (function(module, __unused_webpack_exports, __webpack_require__) {
@@ -8,6 +16,7 @@
 var map = {
 	"./00-settings/config.js": "./src/components/00-settings/config.js",
 	"./03-elements/script.js": "./src/components/03-elements/script.js",
+	"./05-components/globals.js": "./src/components/05-components/globals.js",
 	"./05-components/layout/layout-base.js": "./src/components/05-components/layout/layout-base.js",
 	"./05-components/layout/layout-blank.js": "./src/components/05-components/layout/layout-blank.js",
 	"./05-components/layout/layout-emergency.js": "./src/components/05-components/layout/layout-emergency.js",
@@ -95,8 +104,7 @@ var map = {
 	"./05-components/v3-pat/truncated-content/scripts.js": "./src/components/05-components/v3-pat/truncated-content/scripts.js",
 	"./08-pages/clinicianWorkArea.js": "./src/components/08-pages/clinicianWorkArea.js",
 	"./08-pages/eSignature.js": "./src/components/08-pages/eSignature.js",
-	"./08-pages/remoteAppointment.js": "./src/components/08-pages/remoteAppointment.js",
-	"./globals.js": "./src/components/globals.js"
+	"./08-pages/remoteAppointment.js": "./src/components/08-pages/remoteAppointment.js"
 };
 
 
@@ -178,6 +186,16 @@ document.head.appendChild(style);
     }
   }
 })(jQuery, window, document, SapphireWidgets);
+
+/***/ }),
+
+/***/ "./src/components/05-components/globals.js":
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+const packageVersion = (__webpack_require__("./package.json").version);
+const style1 = 'color: #FFA500; font-weight: bold;';
+console.log(`%cSapphire | ${packageVersion} | ${window.location.pathname}`, style1);
+
 
 /***/ }),
 
@@ -1197,14 +1215,20 @@ window.top.SapphireWidgets.ButtonPending = ButtonPending;
 		if (!allowType) inputEl.readOnly = true;
 
 		const commitValue = (v) => {
-			console.log(`will commitValue... -> ${v}`);
+			console.log(`will commitValue -> [${v}]`);
 
-			// if (inputEl.value === v) return;
 			inputEl.value = v;
-			inputToEmitEl.value = to24h(v);
 
-			inputToEmitEl.dispatchEvent(new Event('change'), { bubbles: true });
-			console.log(`commitValue -> ${inputEl.value} -> ${v} -> ${to24h(v)}`);
+			if (to24h(v) === '') {
+				inputToEmitEl.value = '--';
+			} else {
+				inputToEmitEl.value = to24h(v);
+			}
+
+			setTimeout(() => {
+				inputToEmitEl.dispatchEvent(new Event('change'), { bubbles: true });
+				console.log(`commitValue -> [${inputEl.value}] -> [${inputToEmitEl.value}] -> [${to24h(inputToEmitEl.value)}]`);
+			}, 0);
 		};
 
 		// Assigned by the overlay block (if hasOverlay). Lets earlier handlers
@@ -11799,13 +11823,6 @@ SapphireWidgets.GoNextInput = function(currentInput, nextInputClass) {
 	};
 })(jQuery, window, SapphireWidgets);
 
-
-/***/ }),
-
-/***/ "./src/components/globals.js":
-/***/ (function() {
-
-//SapphireWidgets = window.SapphireWidgets = window.SapphireWidgets || {};
 
 /***/ }),
 
