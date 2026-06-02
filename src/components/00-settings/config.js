c69ee572fce93b74e3d45a27480ec4b15081fbcc
António Carvalho <antonio.carvalho@outsystems.com>
2026-06-02 09:27:30 +0100

    test

--- src/components/00-settings/config.js
+++ src/components/00-settings/config.js
@@ -45,13 +45,13 @@ hideAllOverlays = (initiatorWindow) => {
 };
 
 document.body.addEventListener('click', (event) => {
-	if (event.isTrusted) {
-		const isInsideSearchSD = event.target.closest('.SearchSD');
-		if (isInsideSearchSD) {
-			return;
-		}
-		try {
-			window.top.hideAllOverlays(window);
-		} catch (error) {}
-	}
+	// if (event.isTrusted) {
+	// 	const isInsideSearchSD = event.target.closest('.SearchSD');
+	// 	if (isInsideSearchSD) {
+	// 		return;
+	// 	}
+	// 	try {
+	// 		window.top.hideAllOverlays(window);
+	// 	} catch (error) {}
+	// }
 });
