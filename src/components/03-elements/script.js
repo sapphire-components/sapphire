308a522346430f45f91b7b74d839b7f8fbd3099e
Igor.Kirtak <Igor.Kirtak@esystems.fi>
2026-06-05 17:23:27 +0300

    SPD-4977 Fixed textarea auto resize to work with elements initially hidden, added later or after ajax refresh

--- src/components/03-elements/script.js
+++ src/components/03-elements/script.js
@@ -1,35 +1,60 @@
 /* Component Textarea */
-(function($, window, document, SapphireWidgets) {
-  
-  $(document).ready(function() {
-    CallTextAreaAutoResize();
-  });
+(function ($, window, document, SapphireWidgets) {
+	$(document).ready(function () {
+		CallTextAreaAutoResize();
+	});
 
-  window.CallTextAreaAutoResize = function prepareTextAreaAutoResize(TextAreaId){
-    
-    if(TextAreaId==undefined){
-      var textAreaInput = $('textarea');
-    }else{
-      var textAreaInput = $('#'+TextAreaId);
-    }
-    textAreaInput.each(function(){
-      $(this).attr('rows',2);
+	function CallTextAreaAutoResize(textAreaId) {
+		if (textAreaId) {
+			$('#' + textAreaId)
+				.each(function () {
+					initializeTextArea(this);
+				})
+				.on('focus input', function () {
+					resizeTextArea(this);
+				});
+		} else {
+			$('textarea').each(function () {
+				initializeTextArea(this);
+			});
 
-      $(this)[0].style.minHeight = '70px';
-      resizeTextArea($(this)[0]);
-    });
+			// Bind event to document so that it has effect on elements added later or after Ajax refresh
+			$(document).on('focus input', 'textarea', function () {
+				resizeTextArea(this);
+			});
 
-    textAreaInput.on('input', function(){
-      resizeTextArea($(this)[0]);
-    });
-  }
+			// Observe new elements added to initialize them
+			const observer = new MutationObserver((mutations) => {
+				mutations.forEach((mutation) => {
+					mutation.addedNodes.forEach((node) => {
+						if (node.nodeName === 'TEXTAREA') {
+							initializeTextArea(node);
+						} else if (node.nodeType === 1) {
+							$(node)
+								.find('textarea')
+								.each(function () {
+									initializeTextArea(this);
+								});
+						}
+					});
+				});
+			});
+			observer.observe(document.body, { childList: true, subtree: true });
+		}
+	}
 
-  function resizeTextArea ($textAreaInput) {
-    if($textAreaInput.scrollHeight == 0){
-      $textAreaInput.style.height = '70px';
-    }else{
-      $textAreaInput.style.height = 'auto';
-      $textAreaInput.style.height = $textAreaInput.scrollHeight + 'px';
-    }
-  }
+	function initializeTextArea(textAreaInput) {
+		textAreaInput.rows = 2;
+		textAreaInput.style.minHeight = '70px';
+		resizeTextArea(textAreaInput);
+	}
+
+	function resizeTextArea(textAreaInput) {
+		if (textAreaInput.scrollHeight == 0) {
+			textAreaInput.style.height = '70px';
+		} else {
+			textAreaInput.style.height = 'auto';
+			textAreaInput.style.height = textAreaInput.scrollHeight + 'px';
+		}
+	}
 })(jQuery, window, document, SapphireWidgets);
