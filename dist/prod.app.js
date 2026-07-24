8e3a7a8bb507876231e3904d027ef87907d6d503
António Carvalho <antonio.carvalho@outsystems.com>
2026-07-24 12:52:34 +0100

    New Release

--- dist/prod.app.js
+++ dist/prod.app.js
@@ -1,4 +1,4 @@
-/*! prod.app.js || Version: 5.5.354 || Generated: Tue Jul 21 2026 17:43:48 GMT+0100 (Western European Summer Time) */
+/*! prod.app.js || Version: 5.5.355 || Generated: Fri Jul 24 2026 12:52:30 GMT+0100 (Western European Summer Time) */
 /******/ (function() { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
@@ -6,7 +6,7 @@
 /***/ (function(module) {
 
 "use strict";
-module.exports = /*#__PURE__*/JSON.parse('{"name":"sapphire","version":"5.5.354","description":"Styles and scripts for Sapphire HMS design system.","keywords":["design system","sapphire","hms","style guide","patterns","css","javascript"],"homepage":"https://dev.nordicplatforms.com/StyleGuideV2_UI/Instructions.aspx","repository":{"type":"git","url":"https://github.com/sapphire-components/sapphire"},"author":{"name":"António Carvalho","email":"antonio.carvalho@outsystems.com","url":"https://www.linkedin.com/in/carvalhoantonio/"},"scripts":{"dev":"set NODE_ENV=development && webpack-dev-server --config webpack.dev.js","prod":"set NODE_ENV=production && webpack --config webpack.prod.js","new:build":"npm run prod && git add . && git commit -m \\"New Release\\" --allow-empty && git push origin main","release:patch":"release-it patch --ci","release:minor":"release-it minor --git.requireBranch=main && npm run new:build","release:major":"release-it major --git.requireBranch=main && npm run new:build"},"license":"ISC","devDependencies":{"autoprefixer":"^10.4.23","clean-webpack-plugin":"^4.0.0","css-loader":"^7.1.2","dotenv-cli":"^11.0.0","eslint":"^9.39.2","eslint-config-prettier":"^10.1.8","eslint-plugin-prettier":"^5.5.5","mini-css-extract-plugin":"^2.10.0","postcss-loader":"^8.2.0","prettier":"^3.8.1","prettier-stylelint":"^0.4.2","release-it":"^19.2.4","sass":"^1.97.3","sass-loader":"^16.0.6","style-loader":"^4.0.0","stylelint":"^17.0.0","stylelint-config-standard-scss":"^17.0.0","stylelint-order":"^7.0.1","terser-webpack-plugin":"^5.3.16","webpack":"^5.104.1","webpack-cli":"^6.0.1","webpack-dev-middleware":"^7.4.5","webpack-dev-server":"^5.2.3","webpack-hot-middleware":"^2.26.1","webpack-merge":"^6.0.1"}}');
+module.exports = /*#__PURE__*/JSON.parse('{"name":"sapphire","version":"5.5.355","description":"Styles and scripts for Sapphire HMS design system.","keywords":["design system","sapphire","hms","style guide","patterns","css","javascript"],"homepage":"https://dev.nordicplatforms.com/StyleGuideV2_UI/Instructions.aspx","repository":{"type":"git","url":"https://github.com/sapphire-components/sapphire"},"author":{"name":"António Carvalho","email":"antonio.carvalho@outsystems.com","url":"https://www.linkedin.com/in/carvalhoantonio/"},"scripts":{"dev":"set NODE_ENV=development && webpack-dev-server --config webpack.dev.js","prod":"set NODE_ENV=production && webpack --config webpack.prod.js","new:build":"npm run prod && git add . && git commit -m \\"New Release\\" --allow-empty && git push origin main","release:patch":"release-it patch --ci","release:minor":"release-it minor --git.requireBranch=main && npm run new:build","release:major":"release-it major --git.requireBranch=main && npm run new:build"},"license":"ISC","devDependencies":{"autoprefixer":"^10.4.23","clean-webpack-plugin":"^4.0.0","css-loader":"^7.1.2","dotenv-cli":"^11.0.0","eslint":"^9.39.2","eslint-config-prettier":"^10.1.8","eslint-plugin-prettier":"^5.5.5","mini-css-extract-plugin":"^2.10.0","postcss-loader":"^8.2.0","prettier":"^3.8.1","prettier-stylelint":"^0.4.2","release-it":"^19.2.4","sass":"^1.97.3","sass-loader":"^16.0.6","style-loader":"^4.0.0","stylelint":"^17.0.0","stylelint-config-standard-scss":"^17.0.0","stylelint-order":"^7.0.1","terser-webpack-plugin":"^5.3.16","webpack":"^5.104.1","webpack-cli":"^6.0.1","webpack-dev-middleware":"^7.4.5","webpack-dev-server":"^5.2.3","webpack-hot-middleware":"^2.26.1","webpack-merge":"^6.0.1"}}');
 
 /***/ }),
 
@@ -1714,6 +1714,8 @@ window.top.SapphireWidgets.ButtonPending = ButtonPending;
 /***/ (function() {
 
 class WindowPanel {
+	static instances = new Map();
+
 	anchorEl = null;
 	bindedOpen = this.open.bind(this);
 	closeButton = null;
@@ -1722,6 +1724,7 @@ class WindowPanel {
 	contentMaxHeight = null;
 	customContentEl = null;
 	hasClose = null;
+	identifier = null;
 	initOptions = null;
 	keydownHandler = null;
 	linkToOpen = null;
@@ -1739,12 +1742,19 @@ class WindowPanel {
 
 	constructor(initOptions) {
 		this.initOptions = initOptions;
+		this.identifier = this.initOptions.identifier || null;
 
 		this.widgetEl = document.getElementById(this.initOptions.runtimeId);
 
 		if (!this.widgetEl) {
 			console.warn('WindowPanel element not found', this.initOptions.runtimeId, window.location.pathname);
 			return;
+		}
+
+		if (this.identifier) {
+			this.widgetEl.dataset.identifier = this.identifier;
+			this.widgetEl._windowPanel = this;
+			WindowPanel.instances.set(this.identifier, this);
 		}
 
 		this.closeButton = this.widgetEl.querySelector('.windowpanel-close');
@@ -1868,7 +1878,32 @@ class WindowPanel {
 			window.removeEventListener('keydown', this.keydownHandler);
 			this.keydownHandler = null;
 		}
-		this.tippyInstance.hide();
+		this.removeBackdrop();
+		this.tippyInstance?.hide();
+	}
+
+	static get(identifier) {
+		return WindowPanel.instances.get(identifier) ?? null;
+	}
+
+	static open(identifier) {
+		const instance = WindowPanel.get(identifier);
+		if (!instance) {
+			console.warn('WindowPanel not found for identifier', identifier);
+			return null;
+		}
+		instance.open();
+		return instance;
+	}
+
+	static close(identifier) {
+		const instance = WindowPanel.get(identifier);
+		if (!instance) {
+			console.warn('WindowPanel not found for identifier', identifier);
+			return null;
+		}
+		instance.close();
+		return instance;
 	}
 
 	createTemplate(html) {
@@ -1925,7 +1960,6 @@ class WindowPanel {
 		window.addEventListener(`windowpanel-${this.initOptions.identifier}`, this.panelEventHandler);
 
 		this.cancelButton?.addEventListener('click', (event) => {
-			console.log('cancelButton clicked');
 			event.preventDefault();
 			this.broadcastCustomEvent(`windowpanel-${this.initOptions.identifier}`, { action: 'CLOSE' });
 			this.removeBackdrop();
@@ -1933,7 +1967,6 @@ class WindowPanel {
 		});
 
 		this.yesButton?.addEventListener('click', (event) => {
-			console.log('yesButton clicked');
 			event.preventDefault();
 			this.broadcastCustomEvent(`windowpanel-${this.initOptions.identifier}`, { action: 'YES' });
 			this.removeBackdrop();
@@ -1941,7 +1974,6 @@ class WindowPanel {
 		});
 
 		this.noButton?.addEventListener('click', (event) => {
-			console.log('noButton clicked');
 			event.preventDefault();
 			this.broadcastCustomEvent(`windowpanel-${this.initOptions.identifier}`, { action: 'NO' });
 			this.removeBackdrop();
@@ -1949,7 +1981,6 @@ class WindowPanel {
 		});
 
 		this.closeButton?.addEventListener('click', (event) => {
-			console.log('closeButton clicked');
 			event.preventDefault();
 			this.broadcastCustomEvent(`windowpanel-${this.initOptions.identifier}`, { action: 'CLOSE' });
 			this.removeBackdrop();
@@ -1976,7 +2007,6 @@ class WindowPanel {
 	}
 
 	broadcastCustomEvent(eventName, detail) {
-		console.log('broadcastCustomEvent', eventName, detail);
 		const visited = new Set();
 
 		function visit(win) {
