b9821a202a55ae7b4c0d08d0e018d8403ca52cad
António Carvalho <antonio.carvalho@outsystems.com>
2026-08-17 17:01:39 +0100

    layout popup close event

--- src/components/05-components/layout/layout-popup.js
+++ src/components/05-components/layout/layout-popup.js
@@ -88,9 +88,12 @@
 			if (window.top._iframePopup != undefined || false) {
 				const $closeButton = window.parent.$('.os-internal-ui-dialog-titlebar-close');
 
+				console.log('closeButton', $closeButton);
+
 				$closeButton.removeAttr('href');
 				$closeButton.off('click').on('click', () => {
 					const virtualCloseLink = document.querySelector('.LayoutPopup__close');
+					console.log('virtualCloseLink', virtualCloseLink);
 					virtualCloseLink.click();
 					window.top._iframePopup.SapphireWidgets.SapphirePopup.close();
 				});
