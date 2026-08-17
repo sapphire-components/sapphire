0bc8821b702df1de833a79da40938821ad322639
António Carvalho <antonio.carvalho@outsystems.com>
2026-08-17 16:53:11 +0100

    New Release

--- dist/prod.styleguide.js
+++ dist/prod.styleguide.js
@@ -1,4 +1,4 @@
-/*! prod.styleguide.js || Version: 5.5.357 || Generated: Mon Aug 17 2026 16:48:30 GMT+0100 (Western European Summer Time) */
+/*! prod.styleguide.js || Version: 5.5.358 || Generated: Mon Aug 17 2026 16:53:08 GMT+0100 (Western European Summer Time) */
 /******/ (function() { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
