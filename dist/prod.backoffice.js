540345f46340b0c5217b5cba338828a7089b1c7f
António Carvalho <antonio.carvalho@outsystems.com>
2026-08-17 17:01:50 +0100

    New Release

--- dist/prod.backoffice.js
+++ dist/prod.backoffice.js
@@ -1,4 +1,4 @@
-/*! prod.backoffice.js || Version: 5.5.358 || Generated: Mon Aug 17 2026 16:53:08 GMT+0100 (Western European Summer Time) */
+/*! prod.backoffice.js || Version: 5.5.359 || Generated: Mon Aug 17 2026 17:01:47 GMT+0100 (Western European Summer Time) */
 /******/ (function() { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
